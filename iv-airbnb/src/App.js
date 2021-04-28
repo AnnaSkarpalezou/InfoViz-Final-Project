184e04863a06b36912ca3f650de4952c0233dc39
NotLiu <aliu@lediur.com>
2021-04-28 02:20:49 -0500

    implemente basic Choropleth map

--- iv-airbnb/src/App.js
+++ iv-airbnb/src/App.js
@@ -1,18 +1,34 @@
 import React from "react";
 import "./App.css";
 import GenerateMap from "./components/Map.js";
+import * as d3 from "d3";
+
+function useData(csvPath) {
+  const [dataAll, setData] = React.useState([]);
+  React.useEffect(() => {
+    d3.csv(csvPath).then((data) => {
+      data.forEach((d) => {});
+      setData(data);
+    });
+  }, []);
+
+  console.log(dataAll);
+  return dataAll;
+}
 
 function App() {
   // const [selectedBNB, setSelectedBNB] = React.useState(null);
 
-  const width = 500;
-  const height = 500;
+  const width = 1000;
+  const height = 700;
   const margin = { top: 0, right: 40, bottom: 160, left: 0, gap: 40 };
   const innerWidth = width - margin.left - margin.right - margin.gap;
   const innerHeight = height - margin.top - margin.bottom - margin.gap;
 
   const dataPath = "./data/listings.csv";
   const mapPath = "./data/neighbourhoods.geojson";
+
+  const dataAll = useData(dataPath);
 
   return (
     <div className="App">
@@ -22,7 +38,7 @@ function App() {
           y={margin.top}
           WIDTH={innerWidth / 2}
           HEIGHT={innerHeight + margin.gap}
-          data={dataPath}
+          data={dataAll}
           path={mapPath}
         />
       </div>
--- iv-airbnb/src/components/Map.js
+++ iv-airbnb/src/components/Map.js
@@ -16,17 +16,100 @@ function GenerateMap(props) {
   const {
     x,
     y,
-    data,
     WIDTH,
     HEIGHT,
+    data,
     selectedBNB,
     setSelectedBNB,
     path,
   } = props;
 
   const mapData = useMap(path);
+  let neighbourhoodsCount = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+  ];
 
-  console.log(mapData);
+  data.filter(function (d) {
+    //"青浦区 / Qingpu District"
+    if (d.neighbourhood === "青浦区 / Qingpu District") {
+      neighbourhoodsCount[0].push(d);
+    }
+    //"黄浦区 / Huangpu District"
+    if (d.neighbourhood === "黄浦区 / Huangpu District") {
+      neighbourhoodsCount[1].push(d);
+    }
+    //"浦东新区 / Pudong"
+    if (d.neighbourhood === "浦东新区 / Pudong") {
+      neighbourhoodsCount[2].push(d);
+    }
+    //"杨浦区 / Yangpu District"
+    if (d.neighbourhood === "杨浦区 / Yangpu District") {
+      neighbourhoodsCount[3].push(d);
+    }
+    //"虹口区 / Hongkou District"
+    if (d.neighbourhood === "虹口区 / Hongkou District") {
+      neighbourhoodsCount[4].push(d);
+    }
+    //"静安区 / Jing'an District"
+    if (d.neighbourhood === "静安区 / Jing'an District") {
+      neighbourhoodsCount[5].push(d);
+    }
+    //"宝山区 / Baoshan District"
+    if (d.neighbourhood === "宝山区 / Baoshan District") {
+      neighbourhoodsCount[6].push(d);
+    }
+    //"普陀区 / Putuo District"
+    if (d.neighbourhood === "普陀区 / Putuo District") {
+      neighbourhoodsCount[7].push(d);
+    }
+    //"长宁区 / Changning District"
+    if (d.neighbourhood === "长宁区 / Changning District") {
+      neighbourhoodsCount[8].push(d);
+    }
+    //"徐汇区 / Xuhui District"
+    if (d.neighbourhood === "徐汇区 / Xuhui District") {
+      neighbourhoodsCount[9].push(d);
+    }
+    //"闵行区 / Minhang District"
+    if (d.neighbourhood === "闵行区 / Minhang District") {
+      neighbourhoodsCount[10].push(d);
+    }
+    //"嘉定区 / Jiading District"
+    if (d.neighbourhood === "嘉定区 / Jiading District") {
+      neighbourhoodsCount[11].push(d);
+    }
+    //"崇明区 / Chongming District"
+    if (d.neighbourhood === "崇明区 / Chongming District") {
+      neighbourhoodsCount[12].push(d);
+    }
+    //"奉贤区 / Fengxian District"
+    if (d.neighbourhood === "奉贤区 / Fengxian District") {
+      neighbourhoodsCount[13].push(d);
+    }
+    //"金山区 / Jinshan District"
+    if (d.neighbourhood === "金山区 / Jinshan District") {
+      neighbourhoodsCount[14].push(d);
+    }
+    //"松江区 / Songjiang District"
+    if (d.neighbourhood === "松江区 / Songjiang District") {
+      neighbourhoodsCount[15].push(d);
+    }
+  });
 
   const projection = d3.geoMercator().fitSize([WIDTH, HEIGHT], mapData);
   const mapPathing = d3.geoPath(projection);
@@ -39,16 +122,31 @@ function GenerateMap(props) {
     <svg width={WIDTH} height={HEIGHT}>
       <g transform={`translate(${x}, ${y})`}>
         {mapData.features.map((feature, idx) => {
-          let color = "black";
-          if (idx == 0) {
-            color = "steelblue";
+          let color = "#57068c";
+          let opacity = 1;
+          if (neighbourhoodsCount[idx].length > 10000) {
+            opacity = 1;
+          } else if (neighbourhoodsCount[idx].length > 3000) {
+            opacity = 0.85;
+          } else if (neighbourhoodsCount[idx].length > 2000) {
+            opacity = 0.7;
+          } else if (neighbourhoodsCount[idx].length > 1000) {
+            opacity = 0.55;
+          } else if (neighbourhoodsCount[idx].length > 600) {
+            opacity = 0.4;
+          } else if (neighbourhoodsCount[idx].length > 200) {
+            opacity = 0.25;
+          } else {
+            opacity = 0.1;
           }
+
           return (
             <path
               key={idx + "boundary"}
               className={"boundary"}
               d={mapPathing(feature)}
               fill={color}
+              fillOpacity={opacity}
             />
           );
         })}
