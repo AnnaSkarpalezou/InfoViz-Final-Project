9276b7d587da2f64bd92b6528a2933a299ab26df
NotLiu <aliu@lediur.com>
2021-05-06 02:42:29 -0500

    fix polygonstyling

--- iv-airbnb/src/components/Legend.js
+++ iv-airbnb/src/components/Legend.js
@@ -8,23 +8,23 @@ function Legend(props) {
     <g id="Legend">
       <rect x={x} y={y} width={width} height={height} fill={"#57068c"} />
       <text x={x + width + 10} y={2 * y - height / 2 + 3} fill={"#57068c"}>
-        {">"}10,000
+        {">"}10K
       </text>
       <rect x={x} y={2 * y} width={width} height={height} fill={"#702b9d"} />
       <text x={x + width + 10} y={3 * y - height / 2 + 3} fill={"#57068c"}>
-        10,000 - 3,000
+        10K - 3K
       </text>
       <rect x={x} y={3 * y} width={width} height={height} fill={"#8950ae"} />
       <text x={x + width + 10} y={4 * y - height / 2 + 3} fill={"#57068c"}>
-        3,000 - 2,000
+        3K - 2K
       </text>
       <rect x={x} y={4 * y} width={width} height={height} fill={"#a376c0"} />
       <text x={x + width + 10} y={5 * y - height / 2 + 3} fill={"#57068c"}>
-        2,000 - 1,000
+        2K - 1K
       </text>
       <rect x={x} y={5 * y} width={width} height={height} fill={"#bc9bd1"} />
       <text x={x + width + 10} y={6 * y - height / 2 + 3} fill={"#57068c"}>
-        1,000 - 600
+        1K - 600
       </text>
       <rect x={x} y={6 * y} width={width} height={height} fill={"#d5c1e2"} />
       <text x={x + width + 10} y={7 * y - height / 2 + 3} fill={"#57068c"}>
